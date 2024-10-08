fd07f23ab6b6017978c1fa22dde4860e9a876864
agrueiror <grueiroalex@gmail.com>
2024-10-08 16:15:06 +0200

    perfecto

--- src/main.java
+++ src/main.java
@@ -1,17 +1,26 @@
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.sql.*;
 
 public class main {
-    public static void main(String[] args) throws SQLException {
+    public static void main(String[] args) throws SQLException, IOException {
        MiConector miConector = new MiConector();
        Connection con = miConector.getConnection();
 
         String sql ="SELECT idEmpleado , nombre , apellidos , email , fechaNac , cargo from EMPLEADO";
         Statement statement = con.createStatement();
         ResultSet resultSet = statement.executeQuery(sql);
-
-        while (resultSet.next()) {
-            System.out.println(resultSet.getInt("idEmpleado")+" "+resultSet.getString("nombre")+" "+resultSet.getString("apellidos")+" "+resultSet.getString("email")+" "+resultSet.getString("fechaNac")+" "+resultSet.getString("cargo"));
+        File file = new File("empleados.txt");
+        FileWriter fileWriter= new FileWriter(file);
+        try(BufferedWriter bufferedWriter = new BufferedWriter(fileWriter)){
+            while (resultSet.next()) {
+                bufferedWriter.write(resultSet.getInt("idEmpleado")+" "+resultSet.getString("nombre")+" "+resultSet.getString("apellidos")+" "+resultSet.getString("email")+" "+resultSet.getString("fechaNac")+" "+resultSet.getString("cargo")+"\n");
+            }
         }
+
+
 
     }
 }
